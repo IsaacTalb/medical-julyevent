58f2f53953d06e030de85ca94ab05461d857ae3d
Isaac_Talb <thaungahlaybaw@gmail.com>
2024-06-05 12:01:33 +0700

    need to install cocopod

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'firebase/firebase_auth.dart'; // Add this import
-import 'package:auth_service.dart';
+import 'firebase/auth_service.dart';
 import 'screen/home_screen.dart';
 import 'screen/login-screen.dart';
 
